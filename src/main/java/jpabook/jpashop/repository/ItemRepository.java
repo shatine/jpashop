d2d707aa42bcbdccd42f2cf21361320fd35d8efa
shatine <goldaladin@naver.com>
2022-01-09 15:36:05 +0900

    complete

--- src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -17,7 +17,10 @@ public class ItemRepository {
         if (item.getId() == null) {
             em.persist(item);
         } else {
-            em.merge(item);
+            // select를 통해서 준영속상태를 영속상태로 만든 후,
+            // JPA 의해서 변경감지가 발생하여, 업데이트가 실행 됨.
+            em.merge(item); // findItem을 통해서 영속성 상태에서 관리되는 아이테을 반환 함.
+            // 병합시 주의 : 모든필드를 업데이트 함. 값이 없을 경우, 널값으로 교체된다.
         }
     }
 
